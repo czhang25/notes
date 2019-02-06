4a5725447a11f6f5b98f3c4aacfcd593793d344c
Dr. Gregory DeLozier <gdelozie@kent.edu>
2019-02-06 22:23:56 +0000

    notes abstracted into another file

--- notes_app.py
+++ notes_app.py
@@ -5,22 +5,22 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask(__name__)
+import notes
 
-notes = []
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/notes', methods=['GET'])
 def get_notes():
-    global notes
     message=""
-    return render_template("notes.html", message=message, notes=notes)
+    my_notes = notes.get_notes()
+    return render_template("notes.html", message=message, notes=my_notes)
 
 @app.route('/notes', methods=['POST'])
 def post_notes():
-    global notes
     message="Note = '" + request.form.get("note") + "'"
     note = request.form.get("note")
-    if note != None and note != "" and not (note in notes):
-        notes.append(note)
-    return render_template("notes.html", message=message, notes=notes)
+    if note != None and note != "":
+        notes.add_note(str(note))
+    my_notes = notes.get_notes()
+    return render_template("notes.html", message=message, notes=my_notes)
