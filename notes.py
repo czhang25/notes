ec18779ebad9928fd84c7143c9f83ab9b3a64961
czhang25 <czhang25@kent.edu>
2019-02-07 01:53:26 +0000

    switch to json

--- notes.py
+++ notes.py
@@ -2,28 +2,28 @@
 # note
 
 import os
-import pickle
+import json
 
 the_notes = []
 
 #get the list of notes
 def get_notes():
     global the_notes
-    if os.path.exists("notes.pkl"):
-        with open("notes.pkl","rb") as f:
-            the_notes = pickle.load(f)
+    if os.path.exists("notes.json"):
+        with open("notes.json","rb") as f:
+            the_notes = json.load(f)
     return the_notes
 
 # add a note to the list of notes
 def add_note(note):
     global the_notes
-    if os.path.exists("notes.pkl"):
-        with open("notes.pkl","rb") as f:
-            the_notes = pickle.load(f)
+    if os.path.exists("notes.json"):
+        with open("notes.json","rb") as f:
+            the_notes = json.load(f)
     print("the initial notes")
     print("the notes")
     the_notes.append(note)
     print("the notes")
     print(the_notes)
-    with open("notes.pkl","wb") as f:
-        pickle.dump(the_notes, f)
+    with open("notes.json","wb") as f:
+        json.dump(the_notes, f)
