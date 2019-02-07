7bcd6d7a267f52956f3de559f6557b32eb241499
czhang25 <czhang25@kent.edu>
2019-02-07 04:44:22 +0000

    2/6/2019 end

--- notes.py
+++ notes.py
@@ -1,4 +1,3 @@
-
 # note
 
 import os
@@ -10,7 +9,7 @@ the_notes = []
 def get_notes():
     global the_notes
     if os.path.exists("notes.json"):
-        with open("notes.json","rb") as f:
+        with open("notes.json","r") as f:
             the_notes = json.load(f)
     return the_notes
 
@@ -18,12 +17,12 @@ def get_notes():
 def add_note(note):
     global the_notes
     if os.path.exists("notes.json"):
-        with open("notes.json","rb") as f:
+        with open("notes.json","r") as f:
             the_notes = json.load(f)
     print("the initial notes")
     print("the notes")
     the_notes.append(note)
     print("the notes")
     print(the_notes)
-    with open("notes.json","wb") as f:
+    with open("notes.json","w") as f:
         json.dump(the_notes, f)
